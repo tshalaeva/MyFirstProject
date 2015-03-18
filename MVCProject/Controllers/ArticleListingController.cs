b614cdb6bc2f6ab090f0d9f397105d2b40289b12
tshalaeva <tatyana.shalaeva@firstlinesoftware.com>
2015-03-18 22:25:47 +0300

    Updated mvc project

---
+++ MVCProject/Controllers/ArticleListingController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class ArticleListingController : Controller
+    {
+        //
+        // GET: /ArticleListing/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public RedirectResult GoBack()
+        {
+            return Redirect("~/Home/Index");
+        }
+
+    }
+}
