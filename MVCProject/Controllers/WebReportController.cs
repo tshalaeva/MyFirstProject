a4c520ae25d8c8b05a163989b3b86379eca73990
tshalaeva <tatyana.shalaeva@firstlinesoftware.com>
2015-04-13 20:06:42 +0400

    Updated WebReport page

--- MVCProject/Controllers/WebReportController.cs
+++ MVCProject/Controllers/WebReportController.cs
@@ -12,7 +12,7 @@ namespace MVCProject.Controllers
     {
         //
         // GET: /WebReport/        
-        private readonly Facade m_facade = MvcApplication.Facade;
+        private readonly Facade _mFacade = MvcApplication.Facade;
 
         public ActionResult Index()
         {
@@ -26,7 +26,7 @@ namespace MVCProject.Controllers
             {
                 case "1":
                     {
-                        var articles = m_facade.GetArticles();
+                        var articles = _mFacade.GetArticles();
                         model.Content = string.Format("Title of article {0}: {1}", articles.First().Id,
                             articles.First().Title);
                         for (var i = 1; i < articles.Count; i++)
@@ -35,25 +35,24 @@ namespace MVCProject.Controllers
                                 articles[i].Title);
 
                         }
-                        string pattern = "\\s{2,}";
-                        string replacement = System.Environment.NewLine;
-                        var rgx = new System.Text.RegularExpressions.Regex(pattern);
-                        model.Content = rgx.Replace(model.Content, replacement);
+
+                        model.Content = TrimSpaces(model.Content);
                         break;
                     }
                 case "2":
                     {
-                        var articles = m_facade.GetArticles();
+                        var articles = _mFacade.GetArticles();
                         foreach (var article in articles)
                         {
                             model.Content = string.Format("{0}\nAverage rating of article {1}: {2}", model.Content,
                                 article.Id, article.GetAverageRating());
                         }
+                        model.Content = TrimSpaces(model.Content);
                         break;
                     }
                 case "3":
                 {
-                    var admins = m_facade.GetAdmins();
+                    var admins = _mFacade.GetAdmins();
                     foreach (var admin in admins)
                     {
                         var privilegies = admin.Privilegies.First();
@@ -63,18 +62,19 @@ namespace MVCProject.Controllers
                         }
                         model.Content = string.Format("{0}\nPrivilegies of {1} {2}: {3}", model.Content, admin.FirstName, admin.LastName, privilegies);
                     }
+                    model.Content = TrimSpaces(model.Content);
                     break;
                 }
                 case "4":
                 {
-                    var articles = m_facade.GetArticles();
+                    var articles = _mFacade.GetArticles();
                     model.Content = "List of comments for each article:";
                     foreach (var article in articles)
                     {
                         model.Content = string.Format("{0}\nArticle {1}: ", model.Content, article.Title);                        
-                        var articleComments = m_facade.FilterCommentsByArticle(article).OfType<Comment>().ToList();
-                        var articleReviews = m_facade.FilterCommentsByArticle(article).OfType<Review>().ToList();
-                        var articleReviewTexts = m_facade.FilterCommentsByArticle(article).OfType<ReviewText>().ToList();
+                        var articleComments = _mFacade.FilterCommentsByArticle(article).OfType<Comment>().ToList();
+                        var articleReviews = _mFacade.FilterCommentsByArticle(article).OfType<Review>().ToList();
+                        var articleReviewTexts = _mFacade.FilterCommentsByArticle(article).OfType<ReviewText>().ToList();
                         var allArticleComments = new List<BaseComment>(articleComments.Count + articleReviews.Count + articleReviewTexts.Count);
                         allArticleComments.AddRange(articleComments);
                         allArticleComments.AddRange(articleReviews);
@@ -84,36 +84,47 @@ namespace MVCProject.Controllers
                             model.Content = string.Format("{0}\n{1}", model.Content, comment);
                         }                        
                     }
+                    model.Content = TrimSpaces(model.Content);
                     break;
                 }
                 case "5":
                 {
                     model.Content = "Entity Codes:";
 
-                    foreach (var comment in m_facade.GetComments())
+                    foreach (var comment in _mFacade.GetComments())
                     {
                         model.Content = string.Format("{0}\n{1}: {2}", model.Content, comment.Content, comment.GetEntityCode());
                     }
 
-                    var reviews = m_facade.GetReviews();
+                    var reviews = _mFacade.GetReviews();
                     foreach (var comment in reviews.Where(comment => !(comment is ReviewText)))
                     {
                         model.Content = string.Format("{0}\n{1}: {2}", model.Content, comment.Content, comment.GetEntityCode());
                     }
 
-                    foreach (var comment in m_facade.GetReviewTexts())
+                    foreach (var comment in _mFacade.GetReviewTexts())
                     {
                         model.Content = string.Format("{0}\n{1}: {2}", model.Content, comment.Content, comment.GetEntityCode());
                     }
+                    model.Content = TrimSpaces(model.Content);
                     break;                   
                 }
                 case "6":
                 {
-                    model.Content = string.Format("Random article id = {0}", m_facade.GetRandomArticle().Id);                    
+                    model.Content = string.Format("Random article id = {0}", _mFacade.GetRandomArticle().Id);
+                    model.Content = TrimSpaces(model.Content);                 
                     break;
                 }
             }
             return View(model);
+        }
+
+        private string TrimSpaces(string input)
+        {
+            const string pattern = "\\s{2,}";
+            var replacement = System.Environment.NewLine;
+            var rgx = new System.Text.RegularExpressions.Regex(pattern);
+            return rgx.Replace(input, replacement);
         }
     }
 }
